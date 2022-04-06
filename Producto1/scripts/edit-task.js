3e6a256a3d021a9e239451afc12212b0557905fd
Cesar Perez <cesarperezsampedro@gmail.com>
2022-04-06 20:22:40 +0200

    Trabajo de hoy

--- Producto1/scripts/edit-task.js
+++ Producto1/scripts/edit-task.js
@@ -9,13 +9,30 @@ function editCancel(){
   card.removeAttribute("id");
 }
 
+function editSave(){
+  const card = document.getElementById("editting");
+  //TODO
+  card.removeAttribute("id");
+}
 
 
-///Queda ver como poner los datos por defecto. El panel modal no existe cuando esta funcion comienza.
 function defaultContentModal(){
   const card = document.getElementById("editting");  //obtenemos la tarjeta con id editting
   const titulo = document.getElementById("titleEdit"); //obtenemos el imput del titulo, hay que modificar los id para que no sean los del modal copiado
   const defaultTitle= card.firstElementChild.firstElementChild.textContent; //obtenemos el valor por defecto del titulo de la tarjeta
   titulo.setAttribute("value", defaultTitle);  //introducimos el valor por defecto
-  ///hasta aqui bien, hay que poner el valor original de la descricion, la piroridad y guardar.
+  const descripcion = document.getElementById("descriptionEdit");
+  const defaultDescription= card.firstElementChild.firstElementChild.nextSibling.textContent;
+  descripcion.textContent= defaultDescription;
+  const priority = document.getElementById("priorityEdit");
+  const defaultPriorityElement= card.firstElementChild.firstElementChild.nextSibling.nextSibling.firstElementChild.firstElementChild.firstElementChild;
+  if(defaultPriorityElement.textContent === "Low"){
+    priority.childNodes[1].setAttribute("selected", "selected");
+  }
+  else if(defaultPriorityElement.textContent === "Medium"){
+    priority.childNodes[3].setAttribute("selected", "selected");
+  }
+  else{
+    priority.childNodes[5].setAttribute("selected", "selected");
+  }
 }
