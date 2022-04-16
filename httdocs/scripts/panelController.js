6c0e82403982c9b1ddebf7aeaedda14800254913
oriolSanchez <ori.reina@gmail.com>
2022-04-16 21:08:30 +0200

    .

--- httdocs/scripts/panelController.js
+++ httdocs/scripts/panelController.js
@@ -28,94 +28,91 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 
 
-const Panel = {
-    addPanel(title, description, id) {
-        //Creamos un objeto panel
-        const element = document.getElementById('columns')
-        const col = document.createElement("div")
-        const card = document.createElement("div")
-        const img = document.createElement("img")
-        const cardBody = document.createElement("div")
-        const cardTitle = document.createElement("h5")
-        const cardDescription = document.createElement("p")
-        const cardFooter = document.createElement("div")
-        const deleteButton = document.createElement("button")
-        const deleteButtonContent = document.createElement("i")
-        const goButton = document.createElement("a")
-        const newButton = document.getElementById("new")
-        //Añadimos las clases
-        col.classList.add("col-3", "pt-5")
-        card.classList.add("card", "w-auto", "h-100")
-        img.className = "card-img-top"
-        img.src = 'img/placeholder-image.png'//image.value
-        cardBody.classList.add("card-body", "h-auto")
-        cardTitle.innerHTML = title
-        cardDescription.innerHTML = description
-        cardFooter.classList.add("card-footer", "d-flex", "justify-content-between")
-        deleteButton.classList.add("btn", "btn-danger", "deletePanel")
-        deleteButtonContent.classList.add("bi", "bi-trash")
-        goButton.classList.add("btn", "btn-primary")
-        goButton.href = "/" + id
-        goButton.innerHTML = "Go to panel"
-        //Añadimos función removeElement al deleteButton para que permita eliminar
-        deleteButton.setAttribute("id", id)
-        //deleteButton.setAttribute("onclick", "deletePanel(this.id)")
-        deleteButton.setAttribute("data-bs-toggle", "modal")
-        deleteButton.setAttribute("data-bs-target", "#removeModal")
-        //Añadimos cada nodo a su padre.
-        cardBody.appendChild(cardTitle)
-        cardBody.appendChild(cardDescription)
-        deleteButton.appendChild(deleteButtonContent)
-        cardFooter.appendChild(deleteButton)
-        cardFooter.appendChild(goButton)
-        card.appendChild(img)
-        card.appendChild(cardBody)
-        card.appendChild(cardFooter)
-        col.appendChild(card)
-        element.insertBefore(col, element.firstElementChild)// Añade el panel entre el botón de NEW y el último panel existente
+    const Panel = {
+        addPanel(title, description, id) {
+            //Creamos un objeto panel
+            const element = document.getElementById('columns')
+            const col = document.createElement("div")
+            const card = document.createElement("div")
+            const img = document.createElement("img")
+            const cardBody = document.createElement("div")
+            const cardTitle = document.createElement("h5")
+            const cardDescription = document.createElement("p")
+            const cardFooter = document.createElement("div")
+            const deleteButton = document.createElement("button")
+            const deleteButtonContent = document.createElement("i")
+            const goButton = document.createElement("a")
+            const newButton = document.getElementById("new")
+            //Añadimos las clases
+            col.classList.add("col-3", "pt-5")
+            card.classList.add("card", "w-auto", "h-100")
+            img.className = "card-img-top"
+            img.src = 'img/placeholder-image.png'//image.value
+            cardBody.classList.add("card-body", "h-auto")
+            cardTitle.innerHTML = title
+            cardDescription.innerHTML = description
+            cardFooter.classList.add("card-footer", "d-flex", "justify-content-between")
+            deleteButton.classList.add("btn", "btn-danger", "deletePanel")
+            deleteButtonContent.classList.add("bi", "bi-trash")
+            goButton.classList.add("btn", "btn-primary")
+            goButton.href = "/" + id
+            goButton.innerHTML = "Go to panel"
+            //Añadimos función removeElement al deleteButton para que permita eliminar
+            deleteButton.setAttribute("id", id)
+            deleteButton.setAttribute("onclick", "deletePanel(this.id)")
+            deleteButton.setAttribute("data-bs-toggle", "modal")
+            deleteButton.setAttribute("data-bs-target", "#removeModal")
+            //Añadimos cada nodo a su padre.
+            cardBody.appendChild(cardTitle)
+            cardBody.appendChild(cardDescription)
+            deleteButton.appendChild(deleteButtonContent)
+            cardFooter.appendChild(deleteButton)
+            cardFooter.appendChild(goButton)
+            card.appendChild(img)
+            card.appendChild(cardBody)
+            card.appendChild(cardFooter)
+            col.appendChild(card)
+            element.insertBefore(col, element.firstElementChild)// Añade el panel entre el botón de NEW y el último panel existente
+        }
     }
-}
 
-//Carga de todos los paneles al cargar la página
-fetch('http://localhost:3000/graphql', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-        query: ` query{
+    //Carga de todos los paneles al cargar la página
+    fetch('http://localhost:3000/graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            query: ` query{
                 getAllPanels{
                   titulo
                   descripcion
                   id
                 }
               }`}),
-})
-    .then(res => res.json())
-    .then(res => {
-        for (let i = 0; i < res.data.getAllPanels.length; i++) {
-            let newPanel = new Object(Panel)
-            let titulo = res.data.getAllPanels[i].titulo
-            let descripcion = res.data.getAllPanels[i].descripcion
-            let id = res.data.getAllPanels[i].id;
-            newPanel.addPanel(titulo, descripcion, id)
-            panelArray.push(newPanel)
-        }
     })
-    .catch(err => console.log(err))
-
-document.getElementById("deleteButton").addEventListener('click', function (e){
-    console.log(e.relatedTarget)
+        .then(res => res.json())
+        .then(res => {
+            for (let i = 0; i < res.data.getAllPanels.length; i++) {
+                let newPanel = new Object(Panel)
+                let titulo = res.data.getAllPanels[i].titulo
+                let descripcion = res.data.getAllPanels[i].descripcion
+                let id = res.data.getAllPanels[i].id;
+                newPanel.addPanel(titulo, descripcion, id)
+                panelArray.push(newPanel)
+            }
+        })
+        .catch(err => console.log(err))
 })
 
-function deletePanel(id) {
-    // var modal = new bootstrap.Modal(document.getElementById("removeModal"), {})
-    // modal.show()
+//Delete panels
+function deletePanel(element){
+    console.log(element.id)
     fetch('http://localhost:3000/graphql', {
         method: 'POST',
         headers: { 'Content-Type': 'application/graphql' },
         body:
             `mutation{
-                    deletePanel(id: "${id}"){id}
-                }`
+                deletePanel(id: "${element.id}"){id}
+            }`
     })
         .then(res => res.json())
         .then(res => {
@@ -124,5 +121,3 @@ function deletePanel(id) {
         })
         .catch(err => console.log(err))
 }
-
-});
