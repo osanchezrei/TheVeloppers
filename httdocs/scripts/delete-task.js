c0c2ff92769d6bfe0a6c40531098cb9c8775c786
Cesar Perez <cesarperezsampedro@gmail.com>
2022-04-21 00:12:44 +0200

    Creado y revisado delete task mutation

--- httdocs/scripts/delete-task.js
+++ httdocs/scripts/delete-task.js
@@ -1,14 +1,35 @@
 function setDeleteCard(e){
-  const card = e.parentNode.parentNode.parentNode.parentNode.parentNode;
-  card.setAttribute("id", "deletting");
+  const card = e.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+  card.classList.add("deletting");
+  //card.setAttribute("id", "deletting");
 }
 
+//se ejecuta al cancelar la eliminacion
 function deleteCancel(){
-  const card = document.getElementById("deletting");
-  card.removeAttribute("id");
+  //const card = document.getElementById("deletting");
+  const card = document.getElementsByClassName('deletting')[0];
+  card.classList.remove("deletting");
 }
 
-function deleteTask(){
-  const card = document.getElementById("deletting");
+//se ejecuta al confirmar la eliminacion
+async function deleteTask(){
+  //const card = document.getElementById("deletting");
+  const card = document.getElementsByClassName('deletting')[0];
   card.remove();
+  await deleteTaskDB(card.getAttribute('id'));
+}
+
+//funcion para eliminar la tarea de la base de datos
+function deleteTaskDB(id_tarea){
+  fetch('http://localhost:3000/graphql', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/graphql' },
+    body:
+      `mutation{
+        deleteTarea(id: "${id_tarea}"){
+        id
+      }
+    }`,
+  })
+  .catch(err=> console.log(err))
 }
